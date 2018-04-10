c0679f204f47c4fe5e9e100302a1e890ec9bb63a
Aaron Lampros <alampros@gmail.com>
2018-04-10 15:55:58 -0400

    Simplify

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,27 @@
-'use strict';
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-const path = require('path');
+const path = require('path')
 
 const optionDefaults = {
-  addSassLoader: true
-};
+  addSassLoader: true,
+}
 exports.modifyWebpackConfig = ({ config, _stage }, pluginOptions = optionDefaults) => {
   const {
-    addSassLoader
-  } = pluginOptions;
+    addSassLoader,
+  } = pluginOptions
 
-  const srcDir = path.resolve(config._config.context, 'src');
+  const srcDir = path.resolve(config._config.context, 'src')
 
   const cfg = {
     resolve: {
-      root: srcDir
+      root: srcDir,
     }
-  };
-  if (addSassLoader) {
-    const sassLoader = _extends({}, config.sassLoader || {});
-    const otherPaths = config.sassLoader && config.sassLoader.includePaths || [];
-    cfg.sassLoader = {
-      includePaths: [srcDir, ...otherPaths]
-    };
   }
-  return config.merge(cfg);
-};
+  if(addSassLoader) {
+    const sassLoader = Object.assign({}, config.sassLoader || {})
+    const otherPaths = (config.sassLoader && config.sassLoader.includePaths) || []
+    const includePaths: [srcDir].concat(otherPaths)
+    cfg.sassLoader = {
+      includePaths,
+    }
+  }
+  return config.merge(cfg)
+}
