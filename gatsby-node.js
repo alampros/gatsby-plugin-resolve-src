939b7909607f2bb2b5b8a4dd46ad53f10ecec013
Aaron Lampros <alampros@gmail.com>
2018-04-10 15:41:26 -0400

    Jam source thru babel
    Temporary fix for #3

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,30 @@
-const path = require('path')
-const inspect = obj => require('util').inspect(obj, { colors: true })
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+const path = require('path');
 
 const optionDefaults = {
-  addSassLoader: true,
-}
+  addSassLoader: true
+};
 exports.modifyWebpackConfig = ({ config, _stage }, pluginOptions = optionDefaults) => {
   const {
-    addSassLoader,
-  } = pluginOptions
+    addSassLoader
+  } = pluginOptions;
 
-  const srcDir = path.resolve(config._config.context, 'src')
+  const srcDir = path.resolve(config._config.context, 'src');
 
   const cfg = {
     resolve: {
-      root: srcDir,
+      root: srcDir
     }
-  }
-  if(addSassLoader) {
-    const sassLoader = {
-      ...(config.sassLoader || {}),
-    }
-    const otherPaths = (config.sassLoader && config.sassLoader.includePaths) || []
+  };
+  if (addSassLoader) {
+    const sassLoader = _extends({}, config.sassLoader || {});
+    const otherPaths = config.sassLoader && config.sassLoader.includePaths || [];
     cfg.sassLoader = {
-      includePaths: [srcDir, ...otherPaths],
-    }
+      includePaths: [srcDir, ...otherPaths]
+    };
   }
-  return config.merge(cfg)
-}
+  return config.merge(cfg);
+};
