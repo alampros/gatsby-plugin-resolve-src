b5bab806a252d87274399c8162ca3b4d2c94fd9a
Aaron Lampros <alampros@gmail.com>
2019-02-04 09:59:40 -0500

    v2
    Closes #8

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
+exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, '../../src'), 'node_modules'],
